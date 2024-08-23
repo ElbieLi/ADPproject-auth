af51938d7d69f379b27404125876b0a814f57da5
ElbieLi <zli3@seattleu.edu>
2024-08-23 09:20:19 +0000

    Added comments

--- src/main/java/com/webage/api/TokenAPI.java
+++ src/main/java/com/webage/api/TokenAPI.java
@@ -8,7 +8,6 @@ import java.net.URL;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,16 +22,9 @@ import com.webage.jwt.JWTHelper;
 @RequestMapping("/token")
 public class TokenAPI {
 
-	//private static Key key = AuthFilter.key;	
 	public static Token appUserToken;
 	
-	@GetMapping
-	public String getAll() {
-		return "jwt-fake-token-asdfasdfasfa".toString();
-	}
-	
 	@PostMapping
-	// public ResponseEntity<?> createTokenForCustomer(@RequestBody Customer customer, HttpRequest request, UriComponentsBuilder uri) {
 	public ResponseEntity<?> createTokenForCustomer(@RequestBody Customer customer) {
 		
 		String username = customer.getName();
@@ -43,35 +35,23 @@ public class TokenAPI {
 			ResponseEntity<?> response = ResponseEntity.ok(token);
 			return response;			
 		}
-		// bad request
 		return (ResponseEntity<?>) ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
 		
 	}
 	
 	private boolean checkPassword(String username, String password) {
-		// special case for application user
+		//special case for application user
 		if(username.equals("ApiClientApp") && password.equals("secret")) {
 			return true;
 		}
-		// make call to customer service 
+		//make call to customer service 
 		Customer cust = getCustomerByNameFromCustomerAPI(username);
 		
-		// compare name and password
+		//compare name and password
 		if(cust != null && cust.getName().equals(username) && cust.getPassword().equals(password)) {
 			return true;				
 		}		
 		return false;
-		
-		// local version of the above code, gets customer from repository
-		/*
-		Iterator<Customer> customers = repo.findAll().iterator();
-		while(customers.hasNext()) {
-			Customer cust = customers.next();
-			if(cust.getName().equals(username) && cust.getPassword().equals(password)) {
-				return true;				
-			}
-		}
-		*/
 		
 
 	}
@@ -85,20 +65,11 @@ public class TokenAPI {
 	
     private static Token createToken(String username) {
     	String scopes = "com.webage.data.apis";
-    	// special case for application user
+    	//special case for application user
     	if( username.equalsIgnoreCase("ApiClientApp")) {
     		scopes = "com.webage.auth.apis";
     	}
     	String token_string = JWTHelper.createToken(scopes);
-    	
-		/*
-		 * long fiveHoursInMillis = 1000 * 60 *60 * 5;
-		 * 
-		 * String token_string = Jwts.builder() .setSubject(username)
-		 * .setIssuer("me@me.com") .claim("scopes",scopes) .setExpiration(new
-		 * Date(System.currentTimeMillis() + fiveHoursInMillis)) .signWith(key)
-		 * .compact();
-		 */
     	
     	return new Token(token_string);
     }
